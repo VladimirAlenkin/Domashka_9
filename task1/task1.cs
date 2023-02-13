c72ce34009b4a43be47c4bb2c56afa62e7d7bc95
Vladimir <valenkin@yandex.ru>
2023-02-13 23:09:47 +0500

    task1

---
+++ task1/task1.cs
@@ -0,0 +1,13 @@
+﻿// домашка задача 1
+// Показать натуральные числа от N до 1, N задано
+
+void sequence(int n)
+{   
+    if (n == 0) return;
+    Console.WriteLine(n);
+    sequence(n - 1);
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+sequence(N);
