f1a5b7088c56418bb224d525251be108a0098be0
Vladimir <valenkin@yandex.ru>
2023-02-13 23:28:26 +0500

    task3

---
+++ task3/task3.cs
@@ -0,0 +1,35 @@
+﻿// // домашка задача 3
+// // Найти сумму элементов от M до N, N и M заданы
+
+void amount(int max, int min, int sum)
+{    
+    if (min > max)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + min;
+    min = min + 1;
+    amount(max, min, sum);
+}
+int max;
+int min;
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+if (m > n)
+{
+    max = m;
+    min = n;
+}
+else
+{
+    max = n;
+    min = m;
+}
+int sum = 0;
+Console.Write("Сумма элементов от " + min + " до " + max + " равна: ");
+amount(max, min, sum);
+
+
