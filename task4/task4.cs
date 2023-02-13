2766291e99f8fd87b9906fca931f1ff53f15d7d7
Vladimir <valenkin@yandex.ru>
2023-02-13 23:32:23 +0500

    task4

---
+++ task4/task4.cs
@@ -0,0 +1,21 @@
+﻿// // домашка задача 4
+// // Написать программу вычисления функции Аккермана
+
+int AkkFun (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AkkFun(m - 1, 1);
+    if (m > 0 && n > 0) return AkkFun(m - 1, AkkFun(m, n - 1));
+return AkkFun(m, n);
+}
+
+Console.WriteLine("Введите параметр m числа A:");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите параметр n числа A");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("Функция Аккермана для A (" + m + ", " + n + ") - равна ");
+Console.Write (AkkFun(m, n));
+
+
