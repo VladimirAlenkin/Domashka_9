a3992e89ef903fbc4acdf31518e6b8dc87ff7f78
Vladimir <valenkin@yandex.ru>
2023-02-13 23:36:42 +0500

    task5

---
+++ task5/task5.cs
@@ -0,0 +1,36 @@
+﻿// // домашка задача 5
+// // Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+
+
+void Fibonachi(int m, int k, int sum, int count, int N)
+{
+    sum = m + k;
+    m = k;
+    k = sum;
+    if (count < N)
+    {
+        Console.Write(", ");
+        Console.Write(sum);
+        count++;
+    }
+    else 
+    return;
+    Fibonachi(m, k, sum, count, N);
+}
+
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int k = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество элементов: ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 0;
+int count = 2;
+Console.Write("Последовательность: ");
+Console.Write(m + ", " + k);
+// Console.Write(k);
+Fibonachi(m, k, sum, count, N);
+
+
+
